5bc13ca33807ee940f62bd84686b0fa2e1daa0ab
Stanislav Morozov <r3bfish@gmail.com>
2014-12-05 19:45:45 +0300

    add fonts

--- js/components/popup.jsx
+++ js/components/popup.jsx
@@ -17,7 +17,7 @@ define([], function () {
             if (index !== -1) {
                 var Child = this.props.children,
                     binding = {binding: devices_binding.sub(index)};
-                return <Child binding={binding} />
+                return (<Child binding={binding} />);
             } else {
                 return null;
             }
@@ -27,7 +27,8 @@ define([], function () {
             this.getBinding('showing').set(false);
         },
         render: function () {
-            var cx = React.addons.classSet,
+            var that = this,
+                cx = React.addons.classSet,
                 binding = this.getDefaultBinding(),
                 showing = this.getBinding('showing').toJS(),
                 popover_class_name = cx({
@@ -38,12 +39,28 @@ define([], function () {
                     hidden: !Boolean(showing),
                     overlay: true
                 }),
-                options = this.props.options;
+                options = this.props.options,
+                menu = options.menu ? options.menu.map(function (menu_item) {
+                    var menu_item_classes = cx({
+                        'menu-item': true,
+                        'selected': that.getBinding('selected').get() === menu_item.id
+                    });
+
+                    return (
+                        <span className={menu_item_classes} onClick={options.menu_handler.bind(null, menu_item.id)}>
+                            <span className={'menu-item-icon fa ' + menu_item.icon}></span>
+                            <span className='menu-item-title'>{menu_item.title}</span>
+                        </span>
+                        );
+                }) : null;
 
             return (
                 <div>
                     <div className={popover_class_name}>
-                        <section className='popover-header'>{options.title}</section>
+                        <section className='popover-header'>
+                            {options.title}
+                            {menu ? <span className='menu-container'>{menu}</span> : null}
+                        </section>
                         <section className='popover-content'></section>
                         <section className='popover-footer'>
                             <span className='material-button cancel' onClick={this.onClickCloseButton}>CANCEL</span>
--- js/modules/widgets/common/settings.jsx
+++ js/modules/widgets/common/settings.jsx
@@ -7,6 +7,15 @@ define([
 
     return React.createClass({
         mixins: [Morearty.Mixin],
+        componentWillMount: function () {
+            this.getDefaultBinding().set('widgets.settings.menu_selected', 'settings');
+        },
+        componentWillUnmount: function () {
+            this.getDefaultBinding().remove('widgets.settings.menu_selected');
+        },
+        onSelectedMenuItem: function (id) {
+            this.getDefaultBinding().set('widgets.settings.menu_selected', id);
+        },
         render: function () {
             var binding = this.getDefaultBinding(),
                 devices_binding = this.getBinding('data').sub('devices'),
@@ -16,10 +25,16 @@ define([
                 device_binding = index !== -1 ? devices_binding.sub(index) : null,
                 popup_binding = {
                     default: binding,
-                    showing: binding.sub('widgets.settings.showing')
+                    showing: binding.sub('widgets.settings.showing'),
+                    selected: binding.sub('widgets.settings.menu_selected')
                 },
                 options = {
-                    title: device_binding ? device_binding.get('metrics.title').toUpperCase() : ''
+                    title: device_binding ? device_binding.get('metrics.title').toUpperCase() : '',
+                    menu: [
+                        {id: 'settings', title: 'SETTINGS', icon: 'fa-pie-chart'},
+                        {id: 'statistics', title: 'STATISTICS', icon: 'fa-cogs'}
+                    ],
+                    menu_handler: this.onSelectedMenuItem
                 };
 
             return (
